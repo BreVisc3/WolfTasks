488b79e7b3212c60ecce0c3dfb5f8f15b1364181
bvisc <bvisc@10.152.11.182>
2024-04-01 12:32:02 -0400

    [Implementation] wrote Task class methods, some methods may need work

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/Task.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/Task.java
@@ -1,5 +1,8 @@
 package edu.ncsu.csc216.wolf_tasks.model.tasks;
 
+import edu.ncsu.csc216.wolf_tasks.model.util.ISwapList;
+import edu.ncsu.csc216.wolf_tasks.model.util.SwapList;
+
 /**
  * The task object is a core piece of the program, it will be used
  * to make up task lists. A task knows its name, description, whether
@@ -7,7 +10,7 @@ package edu.ncsu.csc216.wolf_tasks.model.tasks;
  * 
  * @author Brendan_Viscount
  */
-public class Task {
+public class Task implements Cloneable {
 	
 	/** Name of the task */
 	private String taskName;
@@ -18,16 +21,17 @@ public class Task {
 	/** Flag for whether or not the task is active */
 	private boolean active;
 	/** List of tasks */
-	private AbstractTaskList taskLists;
+	private ISwapList<AbstractTaskList> taskLists;
 	
 	/**
 	 * Task constructor
 	 */
 	public Task(String name, String description, boolean recurring, boolean active) {
-		taskName = name;
-		taskDescription = description;
-		this.recurring = recurring;
-		this.active = active;
+		setTaskName(name);
+		setTaskDescription(description);
+		setRecurring(recurring);
+		setActive(active);
+		taskLists = new SwapList<AbstractTaskList>();
 	}
 
 	/**
@@ -41,8 +45,12 @@ public class Task {
 	/**
 	 * Sets the name of the task to the parameter
 	 * @param taskName to set to
+	 * @throws IllegalArgumentException if parameter description is null or empty
 	 */
 	public void setTaskName(String taskName) {
+		if(taskName == null || taskName.isEmpty()) {
+			throw new IllegalArgumentException("Incomplete task information.");
+		}
 		this.taskName = taskName;
 	}
 	
@@ -57,8 +65,12 @@ public class Task {
 	/**
 	 * Sets the description of the task to the parameter
 	 * @param taskDescription to set to
+	 * @throws IllegalArgumentException if parameter description is null
 	 */
 	public void setTaskDescription(String taskDescription) {
+		if(taskDescription == null) {
+			throw new IllegalArgumentException("Incomplete task information.");
+		}
 		this.taskDescription = taskDescription;
 	}
 
@@ -99,7 +111,11 @@ public class Task {
 	 * @return String of the name of the task list
 	 */
 	public String getTaskListName() {
-		return "";
+		if(taskLists.get(0).getTaskListName() == null || taskLists.get(0).getTaskListName().isEmpty()) {
+			return "";
+		}
+		
+		return taskLists.get(0).getTaskListName();
 	}
 	
 	/**
@@ -107,7 +123,20 @@ public class Task {
 	 * @param list to add
 	 */
 	public void addTaskList(AbstractTaskList list) {
+		if(list == null) {
+			throw new IllegalArgumentException("Incomplete task information.");
+		}
 		
+		boolean duplicate = false;
+		for(int i = 0; i < taskLists.size(); i++) {
+			if(taskLists.get(i).getTaskListName().equals(list.getTaskListName())) {
+				duplicate = true;
+			}
+		}
+		
+		if(!duplicate) {
+			taskLists.add(list);
+		}
 	}
 	
 	/**
@@ -115,15 +144,27 @@ public class Task {
 	 */
 	public void completeTask() {
 		
+		for(int i = 0; i < taskLists.size(); i++) {
+			taskLists.get(i).completeTask(this);
+		}
+		
+		if(recurring) {
+			for(int i = 0; i < taskLists.size(); i++) {
+				taskLists.get(i).addTask(this);
+			}
+		}
 	}
 	
 	/**
 	 * Create a copy of the task
 	 * @return a copy of the task
+	 * @throws CloneNotSupportedException 
 	 */
-	public Object clone() {
-		Object s = 0;
-		return s;
+	public Object clone() throws CloneNotSupportedException {
+		if(taskLists == null || taskLists.get(0) == null) {
+			throw new CloneNotSupportedException("Cannot clone.");
+		}
+		return new Task(taskName, taskDescription, recurring, active);
 	}
 	
 	/**
