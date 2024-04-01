b0ec1dab84a59956b59fb8ba8f1b65aec3d41507
bvisc <bvisc@10.152.11.182>
2024-04-01 11:13:13 -0400

    [Implementation] wrote SwapList methods

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
@@ -28,73 +28,107 @@ public class SwapList<E> implements ISwapList<E> {
 	/**
 	 * Adds an element to the list
 	 * @param element to add to the list
+	 * @throws NullPointerException if element parameter is null
 	 */
 	@Override
 	public void add(E element) {
-		// TODO Auto-generated method stub
-		
+		if(element == null) {
+			throw new NullPointerException("Cannot add null element.");
+		}
+		checkCapacity(size - 1);
+		list[size] = element;
+		size++;
 	}
 	
 	/**
 	 * Removes element in index parameter
 	 * @param index to remove
 	 * @return element that was removed
+	 * @throws IllegalArgumentException if index is negative or out of bounds of array
 	 */
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		
+		E value = list[index];
+		for(int i = index; i < size; i++) {
+			list[i] = list[i + 1];
+		}
+		list[size - 1] = null; //Remove duplicate at end
+		size--; //Decrement size
+		return value; //Return removed value
 	}
 
 	/**
 	 * Move an element up in the list
 	 * @param index of element to move
+	 * @throws IllegalArgumentException if index is negative or out of bounds of array
 	 */
 	@Override
 	public void moveUp(int index) {
-		// TODO Auto-generated method stub
+		checkIndex(index);
 		
+		E value = list[index];
+		list[index] = list[index - 1];
+		list[index - 1] = value;
 	}
 
 	/**
 	 * Move an element down in the list
 	 * @param index of element to move
+	 * @throws IllegalArgumentException if index is negative or out of bounds of array
 	 */
 	@Override
 	public void moveDown(int index) {
-		// TODO Auto-generated method stub
+		checkIndex(index);
 		
+		E value = list[index];
+		list[index] = list[index + 1];
+		list[index + 1] = value;
 	}
 
 	/**
 	 * Move an element to the front of the list
 	 * @param index of element to move
+	 * @throws IllegalArgumentException if index is negative or out of bounds of array
 	 */
 	@Override
 	public void moveToFront(int index) {
-		// TODO Auto-generated method stub
+		checkIndex(index);
 		
+		E value = list[index];
+		for(int i = index; i > 0; i--) {
+			list[i] = list[i - 1];
+		}
+		list[0] = value;
 	}
 
 	/**
 	 * Move an element to the back of the list
 	 * @param index of element to move
+	 * @throws IllegalArgumentException if index is negative or out of bounds of array
 	 */
 	@Override
 	public void moveToBack(int index) {
-		// TODO Auto-generated method stub
+		checkIndex(index);
 		
+		E value = list[index];
+		for(int i = index; i < size - 1; i++) {
+			list[i] = list[i + 1];
+		}
+		list[size - 1] = value;
 	}
 
 	/**
 	 * Returns the element in the parameter index
 	 * @param index of element to get
 	 * @return the element in parameter index
+	 * @throws IllegalArgumentExcpetion if index is negative or out of bounds of the array
 	 */
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		return list[index];
 	}
 
 	/**
@@ -110,16 +144,26 @@ public class SwapList<E> implements ISwapList<E> {
 	 * Ensures the capacity of the array can hold another element before adding
 	 * @param index where the element is to be added
 	 */
+	@SuppressWarnings("unchecked")
 	private void checkCapacity(int index) {
-		
+		if(index >= list.length - 2) {
+			E[] temp = list.clone();
+			list = (E[])new Object[list.length * 2];
+			for(int i = 0; i < size; i++) {
+				list[i] = temp[i];
+			}
+		}
 	}
 	
 	/**
 	 * Checks the contents of parameter index
 	 * @param index to check
+	 * @throws IllegalArgumentException if index is out of the bounds of the array
 	 */
 	private void checkIndex(int index) {
-		
+		if(index < 0 || index >= size) {
+			throw new IllegalArgumentException("Invalid index.");
+		}
 	}
 	
 
