3fcc053bdc0ec13a3b2d428ec0cfb02f0d4f71b3
bvisc <bvisc@10.152.25.66>
2024-04-03 13:30:49 -0400

    [Implementation] Began writing FileWriter implementation

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import java.io.File;
+import java.io.FileWriter;
 
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
 import edu.ncsu.csc216.wolf_tasks.model.util.ISortedList;
@@ -23,8 +24,14 @@ public class NotebookWriter {
 	/**
 	 * Writes the information stored in a taskList into a file
 	 * @param fileName of File to save information into
+	 * @param notebookName of notebook
+	 * @param list taskLists in notebook
 	 */
-	public static void writeNotebookFile(File fileName, String name, ISortedList<TaskList> list) {
-		
+	public static void writeNotebookFile(File fileName, String notebookName, ISortedList<TaskList> list) {
+		try {
+			FileWriter write = new FileWriter(fileName):
+		} catch(Exception e) {
+			throw new IllegalArgumentException("Unable to save file.");
+		}
 	}
 }
